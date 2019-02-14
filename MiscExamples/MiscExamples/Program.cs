238475d929f713d19bd8597e0943d054322173c2
Sergey Bolotov <sergey.bolotov@rokolabs.com>
2019-02-14 17:07:05 +0400

    Added project "MiscExamples"

---
+++ MiscExamples/MiscExamples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscExamples
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int value    = 0x00001010;
+			int checkBit = 0x00000010;
+			bool hasBit = (value & checkBit) == checkBit;
+
+			Console.WriteLine(hasBit);
+			Console.ReadLine();
+		}
+	}
+}
