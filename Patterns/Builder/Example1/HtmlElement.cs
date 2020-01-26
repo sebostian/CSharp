daf3f286b4ee6a86418957a55129ddbd3dab400e
sebostian <bessarab27@gmail.com>
2020-01-27 02:08:52 +0400

    Added part of implementation

---
+++ Patterns/Builder/Example1/HtmlElement.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder.Example1
+{
+	public class HtmlElement
+	{
+		private const int indentSize = 2;
+		private StringBuilder sb = new StringBuilder();
+		public string Name { get; set; }
+		public string Text { get; set; }
+
+		public List<HtmlElement> Elements = new List<HtmlElement>();
+
+		public HtmlElement()
+		{
+		}
+
+		public HtmlElement(string name, string text)
+		{
+			Name = name;
+			Text = text;
+		}
+		public override string ToString()
+		{
+			if (Text == null)
+			{
+				sb.AppendLine($"<{Name}>");
+			}
+			else
+			{
+				sb.Append($"<{Name}>{Text}");
+			}
+
+			foreach (var item in Elements)
+			{
+				sb.Append(new String(' ', indentSize));
+				sb.Append(item.ToString());
+			}
+
+			sb.AppendLine($"</{Name}>");
+
+			return sb.ToString();
+		}
+	}
+}
--- Patterns/Builder/Program.cs
+++ Patterns/Builder/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Builder.Example1;
+using System;
 
 namespace Builder
 {
+	// The Builder pattern is concerned with the creation of complicated objects;
+	// that is, objects that cannot be built up in a single-line constructor call.
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			DoTestExample1();
+		}
+
+		private static void DoTestExample1()
+		{
+			var words = new[] { "hello", "world" };
+			var tag = new HtmlElement("ul", null);
+			foreach (var word in words)
+			{
+				tag.Elements.Add(new HtmlElement("li", word));
+			}
+
+			Console.WriteLine(tag); // calls tag.ToString()
 		}
 	}
 }
